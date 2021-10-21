3c28c68f174102d32c916931c6bcdb9fcca180d2
FuzzyCookie97 <69955315+FuzzyCookie97@users.noreply.github.com>
2021-10-21 19:07:49 +0200

    New layout work

---
+++ QuizAppNav/app/src/main/java/com/example/quizappnav/StarterPage.java
@@ -0,0 +1,39 @@
+package com.example.quizappnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StarterPage extends AppCompatActivity {
+
+    Button login;
+    Button newUser;
+    Button kode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starter_page);
+
+        login = findViewById(R.id.loginButton);
+        newUser = findViewById(R.id.newUser);
+        kode = findViewById(R.id.kode);
+
+        login.setOnClickListener((view -> {
+            Intent logger = new Intent(getApplicationContext(),LoginFrag.class);
+            startActivity(logger);
+        }));
+
+        newUser.setOnClickListener((view -> {
+            Intent newUserLog = new Intent(getApplicationContext(),LoginFrag.class);
+            startActivity(newUserLog);
+        }));
+
+        kode.setOnClickListener((view -> {
+            Intent kodeLog = new Intent(getApplicationContext(),LoginFrag.class);
+            startActivity(kodeLog);
+        }));
+    }
+}
