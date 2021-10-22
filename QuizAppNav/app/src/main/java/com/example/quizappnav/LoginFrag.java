5a71bb7fb0bc4f459a4979667ba13966f42333fa
FuzzyCookie97 <69955315+FuzzyCookie97@users.noreply.github.com>
2021-10-22 17:55:45 +0200

    Added a username and password check
    hardcoded example

--- QuizAppNav/app/src/main/java/com/example/quizappnav/LoginFrag.java
+++ QuizAppNav/app/src/main/java/com/example/quizappnav/LoginFrag.java
@@ -1,5 +1,6 @@
 package com.example.quizappnav;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -18,17 +19,36 @@ import android.widget.Toast;
 public class LoginFrag extends AppCompatActivity {
 
 Button login;
+EditText username;
+EditText password;
+
+Context context;
+CharSequence text;
+Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frag_login);
 
         login = findViewById(R.id.loginButton);
+        username = findViewById(R.id.loginUserName);
+        password = findViewById(R.id.loginPassword);
 
+        context = getApplicationContext();
+        text = "Username or Password incorrect";
+        toast = Toast.makeText(context,text,Toast.LENGTH_SHORT );
 
         login.setOnClickListener((view -> {
-            Intent browse = new Intent(getApplicationContext(),HomeFrag.class);
-            startActivity(browse);
+            if (username.getText().toString().equals("test")  &&
+                    password.getText().toString().equals("123")) {
+                Intent browse = new Intent(getApplicationContext(), HomeFrag.class);
+                startActivity(browse);
+            }
+            else{
+                toast.show();
+            }
         }));
-    }
+
+
+}
 }
